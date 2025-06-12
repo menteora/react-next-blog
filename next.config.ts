ce89a09ee4f3d429afe59a88b9759e82296bb41f
Luca D'Amico <menteora@gmail.com>
2025-06-12 10:27:40 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/react-next-blog',
+  output: 'export',
 };
 
 export default nextConfig;
